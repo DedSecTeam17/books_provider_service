ae5db9596dc040c71cfd99df632322abb151ed70
Mohammed_Elmain <melamin100@yahoo.com>
2019-04-15 21:27:27 +0800

    Auth And Profile

--- routes/user.js
+++ routes/user.js
@@ -12,11 +12,11 @@ const  UsersController=require('../controllers/user');
 //user_id
 
 
-router.post('/users/:user_id/profile',restify_jwt({secret: process.env.JWT_SECRET}), UsersController.createProfile);
-router.post('/users/:user_id/profile/upload_profile_image',restify_jwt({secret: process.env.JWT_SECRET}), UsersController.uploadProfileImage);
+router.post('/users/:user_id/profile',restify_jwt({secret: process.env.JWT_SECRET || 'secret'}), UsersController.createProfile);
+router.post('/users/:user_id/profile/upload_profile_image',restify_jwt({secret: process.env.JWT_SECRET || 'secret'}), UsersController.uploadProfileImage);
 router.post('/users', UsersController.signUp);
 router.post('/auth', UsersController.signIn);
-router.get('/users', restify_jwt({secret: process.env.JWT_SECRET}),UsersController.getUser);
+router.get('/users', restify_jwt({secret: process.env.JWT_SECRET || 'secret'}),UsersController.getUser);
 router.get('/users/profile_image/:image_name',UsersController.getProfileImage);
 router.post('/users/password_reset',UsersController.passwordReset);
 router.post('/users/password_change/:token',UsersController.passwordChange);
